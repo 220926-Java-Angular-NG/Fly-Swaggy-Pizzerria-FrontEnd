91baee866d3325de87686ea8600bf4b845b9140f
NedTurner613 <nedturner613@gmail.com>
2022-10-31 15:52:10 -0500

    created message service

--- pizzerria/src/app/services/register.service.ts
+++ pizzerria/src/app/services/register.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from '../components/models/user';
+import { catchError, map, tap, Observable, of } from 'rxjs';
+import { MessagesService } from './messages.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  private registerURL = '/register';
+  private httpOptions = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json'
+      }
+    )
+  }
+
+  private log(message:string){
+    this.messageService.add(`Hero Service${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T){
+    return (error: any) : Observable<T> => {
+      console.log(error);
+      this.log(`${operation} failed: ${error.message}`);
+      //let the app keep running by returning an empty result
+      return of(result as T);
+    }
+  }
+
+  createUser(user: User){
+    return this.http.post<User>(this.registerURL, user, this.httpOptions).pipe(
+      tap((newUser: User) => this.log(`created user with email ${newUser.email}`)),
+      catchError(this.handleError<User>('createUser'))
+    )
+  }
+
+  constructor(private messageService: MessagesService, private http: HttpClient) { }
 }
