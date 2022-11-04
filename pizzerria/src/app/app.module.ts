04cb5aa6f27de45b755e959de57b90aa41d46016
NedTurner613 <nedturner613@gmail.com>
2022-11-04 16:14:18 -0500

    "got components working"

--- pizzerria/src/app/app.module.ts
+++ pizzerria/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- pizzerria/src/app/components/register/register.component.ts
+++ pizzerria/src/app/components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RegisterService } from 'src/app/services/register.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-register',
