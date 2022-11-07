88c3686f0c20994915267fa246d2fcacb8feaad0
Logan <logan.tan@wsu.edu>
2022-11-06 16:07:03 -0800

    Basic Menu setup Done, Functions need to be created And Radio buttons need to be wired to something Also display price

--- pizzerria/src/app/components/menu/menu.component.ts
+++ pizzerria/src/app/components/menu/menu.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SPECALTY } from '../models/SpecaltyRecipes';
+import { Recipe } from '../models/Recipe';
+import { PizzaBox } from '../models/pizzaBox';
+import { Size, Crust } from '../models/pizza';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  specals: Recipe[] = SPECALTY;
+  size: Size = Size.SMALL;
+  crust: Crust = Crust.REGULAR;
+  add(recipe:Recipe){
 
-
-  
-
+  }
   constructor() { }
 
   ngOnInit(): void {
